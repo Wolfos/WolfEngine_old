91f050782ff904319376bf341a67a75d3fca76c8
Robin van Ee <rvanee@wolfengine.net>
2014-05-02 10:55:44 +0200

    The Mac port is useable now.

--- IKC/Code/WolfEngine/Rendering/Image.cpp
+++ IKC/Code/WolfEngine/Rendering/Image.cpp
@@ -7,32 +7,38 @@ rvanee@wolfengine.net
 #define _CRT_SECURE_NO_DEPRECATE //MICROSOOOOOOOOFT!
 #include "Image.h"
 #include "../Utilities/Debug.h"
-#include <string.h>
 
 #ifdef __APPLE__
-#include <mach-o/dyld.h>
+#include "CoreFoundation/CoreFoundation.h"
 #endif
 
 
-SDL_Texture* Image::Load(char* filename, SDL_Renderer* renderer)
+SDL_Texture* Image::Load(std::string filename, SDL_Renderer* renderer)
 {
 	//The final texture
 	SDL_Texture* newTexture = NULL;
 #ifdef ANDROID
-	char newFilename[1024] = "Sprites/";
+    std::string newFilename = "Sprites/";
+    newFilename += filename;
 #elif defined __APPLE__
-    char newFilename[1024] = "WolfEngine.app/Contents/Resources/Assets/Sprites/";
+    //Find the resources folder
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[PATH_MAX];
+    CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX);
+    std::string cppPath(path);
+    std::string newFilename = cppPath + "/Assets/Sprites/" + filename;
 #else
-	char newFilename[1024] = "../Assets/Sprites/";
+	std::string newFilename = "../Assets/Sprites/";
+    newFilename += filename;
 #endif
-	strcat(newFilename, filename);
+    
 
 	//Load image at specified path
-	SDL_Surface* loadedSurface = IMG_Load(newFilename);
+	SDL_Surface* loadedSurface = IMG_Load(newFilename.c_str());
 	if (!loadedSurface)
 	{
-		printf("Unable to load image %s! SDL_image Error: %s\n", newFilename, IMG_GetError());
-		Debug::Log("Unable to load image");
+		printf("Unable to load image %s! SDL_image Error: %s\n", newFilename.c_str(), IMG_GetError());
 	}
 	else
 	{
@@ -40,8 +46,7 @@ SDL_Texture* Image::Load(char* filename, SDL_Renderer* renderer)
 		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
 		if (!newTexture)
 		{
-			printf("Unable to create texture from %s! SDL Error: %s\n", newFilename, SDL_GetError());
-			Debug::Log("Unable to create texture");
+			printf("Unable to create texture from %s! SDL Error: %s\n", newFilename.c_str(), SDL_GetError());
 		}
 
 		//Get rid of old loaded surface
--- IKC/Code/WolfEngine/Rendering/Image.h
+++ IKC/Code/WolfEngine/Rendering/Image.h
@@ -1,6 +1,7 @@
 #ifndef _IMAGE_H
 #define _IMAGE_H
 #include "../Includes.h"
+#include <string>
 
 ///
 /// Contains functions regarding the loading of images from file
@@ -8,6 +9,6 @@
 class Image{
 	public:
 		///	Returns a pointer to an SDL_Texture from a filename
-		static SDL_Texture* Load(char* filename, SDL_Renderer* renderer);
+    static SDL_Texture* Load(std::string filename, SDL_Renderer* renderer);
 };
 #endif
