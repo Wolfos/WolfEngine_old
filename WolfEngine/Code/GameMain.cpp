ddd206a40b9e62bc030593bbf830140be6ff9c1e
Robin van Ee <robin.vanee@gmail.com>
2014-05-29 14:28:52 +0200

    Empty game project

--- WolfEngine/Code/GameMain.cpp
+++ WolfEngine/Code/GameMain.cpp
@@ -1,20 +1,17 @@
 #include "GameMain.h"
 #include "WolfEngine/WolfEngine.h"
-#include "GameManager.h"
-#include "MiniGames/Memory.h"
-#include "MiniGames/MainMenu.h"
 
 void GameMain::Start()
 {
-	GameManager::StartMiniGame<MainMenu>();
+	
 }
 
 void GameMain::Update()
 {
-	GameManager::UpdateMiniGame();
+	
 }
 
 void GameMain::Exit()
 {
-	GameManager::ExitMiniGame();
+	
 }
